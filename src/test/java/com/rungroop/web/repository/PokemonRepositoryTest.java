5b26bc14b978836b35d3b3fc0e0a872a8158b20a
Tanish-5007 <tanish.ee19@nsut.ac.in>
2023-07-14 12:42:22 +0530

    delete impl

---
+++ src/test/java/com/rungroop/web/repository/PokemonRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.rungroop.web.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PokemonRepositoryTest {
+
+	@Test
+	public void test() {
+		assertEquals(1,	1);
+	}
+
+}
